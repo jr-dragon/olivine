811d030046dc1ff5a8f1f7924885178e4f745783
J. Dragon <j.dragon@chivincent.net>
2026-07-21 18:12:19 +0800

    feat: add PPROF_MUTEX_FRAC and PPROF_BLOCK_RATE to enable profiling with blocking and mutexing

--- cmd/olivine/main.go
+++ cmd/olivine/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"log/slog"
+	"os"
+	"runtime"
+	"strconv"
 
 	"olivine/internal/data"
 )
 
 func main() {
+	if os.Getenv("PPROF_MUTEX_FRAC") != "" {
+		if rate, err := strconv.Atoi(os.Getenv("PPROF_MUTEX_FRAC")); err != nil {
+			panic("PPROF_MUTEX_FRAC must be integer: " + err.Error())
+		} else {
+			runtime.SetMutexProfileFraction(rate)
+		}
+	}
+
+	if os.Getenv("PPROF_BLOCK_RATE") != "" {
+		if rate, err := strconv.Atoi(os.Getenv("PPROF_BLOCK_RATE")); err != nil {
+			panic("PPROF_BLOCK_RATE must be integer: " + err.Error())
+		} else {
+			runtime.SetBlockProfileRate(rate)
+		}
+	}
+
 	cfg, err := data.NewConfig("redis.conf")
 	if err != nil {
 		slog.Error("failed to load config", slog.Any("error", err))
